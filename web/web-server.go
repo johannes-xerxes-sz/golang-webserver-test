090b96fd1a0c9670baad49b8b60420585ebecb71
johannes-xerxes-sz <johannes.xerxes13@gmail.com>
2024-03-13 10:47:02 +0800

    added fiber webserver

--- web/web-server.go
+++ web/web-server.go
@@ -1,19 +1,31 @@
 package main
 
-import (
-	"net/http"
+// import (
+// 	"net/http"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
+
+// func main() {
+// 	r := gin.Default()
+// 	r.GET("/", func(c *gin.Context) {
+// 		c.String(http.StatusOK, "Hello, World!")
+// 	})
+// 	r.Run(":8080")
+// }
+
+// func handler(c *gin.Context) {
+// 	c.String(http.StatusOK, "Hello, World!")
+// }
+
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello, World!")
-	})
-	r.Run(":8080")
-}
+	app := fiber.New()
 
-func handler(c *gin.Context) {
-	c.String(http.StatusOK, "Hello, World!")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, Fiber!")
+	})
+
+	app.Listen((":8080"))
 }
